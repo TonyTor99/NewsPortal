74a50a6c994b43f5894cb0574b94da6272f2f3af
TonyTor99 <shpenov1@gmail.com>
2024-11-27 22:06:30 +0500

    Настроил отображение модели Post в админ-панели

--- news/admin.py
+++ news/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 from .models import Author, Post, Category, Comment
 
 
+def nullfy_rating(modeladmin, request, queryset):
+    queryset.update(rating_post=0)
+
+
+nullfy_rating.short_description = 'Обнулить рейтинг поста'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'categories', 'news_article')
+    list_filter = ('news_article', 'title', 'time_in', 'author', 'category')
+    search_fields = ('title', 'text', 'category__cat_name')
+    actions = [nullfy_rating]
+
+    def categories(self, obj):
+        return ', '.join([category.cat_name for category in obj.category.all()])
+
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
