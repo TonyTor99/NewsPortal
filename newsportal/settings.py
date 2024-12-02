e2597ab61a3d05cc75098746980d13811a0fa593
TonyTor99 <shpenov1@gmail.com>
2024-12-02 21:36:04 +0500

    Подключил БД PostgreSQL и загрузил в нее все данные из прошлой БД

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -85,9 +85,13 @@ WSGI_APPLICATION = 'newsportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'News',
+        'USER': 'postgres',
+        'PASSWORD': '2252',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 # Password validation
