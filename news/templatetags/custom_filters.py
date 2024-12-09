54f6c1dd62abd249e43d31bce793f1cfeab036a4
TonyTor99 <shpenov1@gmail.com>
2024-12-09 21:13:18 +0500

    1)Написал катстомный тег цензуры текста 2)Настроил логгирование

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,20 +1,34 @@
 from django import template
 import re
 
+from news.templatetags.forbidden_words import forbidden_words
+
 
 register = template.Library()
 
 
+# @register.filter()
+# def censor(value):
+#     try:
+#         def replace_with_stars(match):
+#             word = match.group(0)
+#             return f'{word[0]}{(int(len(word)) - 1) * '*'}'
+#
+#         # Регулярное выражение для поиска слов, начинающихся с заглавной буквы
+#         censored_text = re.sub(r'\b[A-ZА-Я][a-zа-яё]*\b', replace_with_stars, value)
+#
+#         return censored_text
+#     except TypeError:
+#         print('Введены данные не в формате текста')
+
+
 @register.filter()
 def censor(value):
-    try:
-        def replace_with_stars(match):
-            word = match.group(0)
-            return f'{word[0]}{(int(len(word)) - 1) * '*'}'
-
-        # Регулярное выражение для поиска слов, начинающихся с заглавной буквы
-        censored_text = re.sub(r'\b[A-ZА-Я][a-zа-яё]*\b', replace_with_stars, value)
-
-        return censored_text
-    except TypeError:
-        print('Введены данные не в формате текста')
+    words = value.split()
+    consed_text = []
+    for word in words:
+        if word in forbidden_words:
+            consed_text.append('*' + word[1:-1] + '*')
+        else:
+            consed_text.append(word)
+    return ' '.join(consed_text)
--- newsportal/settings.py
+++ newsportal/settings.py
@@ -177,3 +177,109 @@ CACHES = {
         'TIMEOUT': 100,
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+        'console_with_path': {
+            'format': '{asctime} {levelname} {message} (Path: {pathname})',
+            'style': '{',
+        },
+        'file_general': {
+            'format': '{asctime} {levelname} [{module}] {message}',
+            'style': '{',
+        },
+        'file_error': {
+            'format': '{asctime} {levelname} {message} (Path: {pathname})',
+            'style': '{',
+        },
+        'security': {
+            'format': '{asctime} {levelname} [{module}] {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'debug_only': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+        'production_only': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['debug_only'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+        'console_warning': {
+            'level': 'WARNING',
+            'filters': ['debug_only'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_with_path',
+        },
+        'file_general': {
+            'level': 'INFO',
+            'filters': ['production_only'],
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'general.log'),
+            'formatter': 'file_general',
+        },
+        'file_error': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'errors.log'),
+            'formatter': 'file_error',
+        },
+        'file_security': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'security.log'),
+            'formatter': 'security',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['production_only'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': False,
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'console_warning', 'file_general'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console_warning', 'file_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['console_warning', 'file_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.template': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['file_security'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
